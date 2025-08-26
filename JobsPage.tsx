eb839a1fe9720ca10d9a9d9d9294852c77924f68
Benson Zino Atedezi <Cuyno957@gmail.com>
2025-08-26 02:33:56 +0000

    Update JobsPage.tsx

--- JobsPage.tsx
+++ JobsPage.tsx
@@ -5,7 +5,7 @@ import { Card } from './components/ui/Card';
 import { Button } from './components/ui/Button';
 import { Input } from './components/ui/Input';
 import { Modal } from './components/ui/Modal';
-import { useAuthContext } from '../contexts/AuthContext';
+import { useAuthContext } from './src/contexts/AuthContext';
 import type { Job } from './types';
 
 const sampleJobs: Job[] = [
