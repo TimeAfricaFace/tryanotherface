63b1f04d8c7cdd54e7f8a60837a062a2c0b490b3
Benson Zino Atedezi <Cuyno957@gmail.com>
2025-08-28 02:26:26 +0000

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import AppRouter from "./AppRouter";
+import AppRouter from './AppRouter';
 import { AuthProvider } from "./AuthContext";
 
 createRoot(document.getElementById("root")!).render(
